5d4f0e37b827a7b0588a7b1718afc0c38a1ec2c0
MpethiX <duse0187@gmail.com>
2019-05-17 11:07:54 +0000

    getting articles

--- test_app/config/routes.rb
+++ test_app/config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  get 'pages/home' => 'pages#home'
  get 'pages/index' => 'pages#index'
-  get 'pages/about' => 'pages#about'
-  resources :articles
+  get 'pages/try' => 'pages#try'
 end
