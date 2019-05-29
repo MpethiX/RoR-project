f31d28118f7436a156a3c7463559da8d3131f783
MpethiX <duse0187@gmail.com>
2019-05-29 12:46:33 +0000

    Fix tests and updated navbar wirh admin resdtriction

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -11,8 +11,9 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     end
     
     test "should get new" do
+        sign_in_as(@user, "password")
         get new_category_path
-                assert_response :success
+        assert_response :success
     end
     test "should get show" do
         sign_in_as(@user, "password")
@@ -23,7 +24,6 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     test "should redirect create when admin not logged in" do
         assert_no_difference 'Category.count' do
             post categories_path, params: {category: {name: "sports"}}
-            follow_redirect!
         end
         assert_redirected_to categories_path
     end
